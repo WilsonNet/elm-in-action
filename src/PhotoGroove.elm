27251eade614866bc2ea89ff2d9c7d5b37c47a8a
Wilson Neto <wilsonpsn@hotmail.com>
2021-09-29 22:32:10 -0300

    feat: advance the project
    advanced the project to use custom types for messages
    and added an radio innput that change size

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -2,9 +2,10 @@ module PhotoGroove exposing (main)
 
 import Array exposing (Array)
 import Browser
-import Html exposing (div, h1, img, text)
+import Html exposing (Html, button, div, h1, img, input, label, small, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Html.Events exposing (onInput)
 
 
 
@@ -27,8 +28,14 @@ type alias Photo =
     { url : String }
 
 
+type ThumbnailSize
+    = Small
+    | Medium
+    | Large
+
+
 type alias Model =
-    { photos : List Photo, selectedUrl : String }
+    { photos : List Photo, selectedUrl : String, choosenSize : ThumbnailSize }
 
 
 initialModel : Model
@@ -39,6 +46,7 @@ initialModel =
         , { url = "3.jpeg" }
         ]
     , selectedUrl = "1.jpeg"
+    , choosenSize = Large
     }
 
 
@@ -46,27 +54,37 @@ initialModel =
 -- UPDATE
 
 
-type alias Msg =
-    { description : String, data : String }
+type Msg =
+    ClickedPhoto String
+    | ClickedSize ThumbnailSize
+    | ClickedSurpriseMe
 
 
 update : Msg -> Model -> Model
 update msg model =
-    if msg.description == "ClickedPhoto" then
-        { model | selectedUrl = msg.data }
-
-    else
-        model
+    case msg of
+        ClickedPhoto url ->
+            { model | selectedUrl = url }
+        ClickedSurpriseMe ->
+            { model | selectedUrl = "2.jpeg" }
+        ClickedSize size->
+            { model | choosenSize = size  }
 
 
 
 -- VIEW
 
 
+view : Model -> Html Msg
 view model =
     div [ class "container" ]
         [ h1 [] [ text "Photos Grooves" ]
-        , div [ id "thumbnails" ] (List.map (viewThumbnail model.selectedUrl) model.photos)
+        , button
+            [ onClick  ClickedSurpriseMe  ]
+            [ text "Surpresinha!" ]
+        , div [ id "choose-size" ]
+            (List.map viewSizeChooser [ Small, Medium, Large ])
+        , div [ id "thumbnails", class (sizeToString model.choosenSize) ] (List.map (viewThumbnail model.selectedUrl) model.photos)
         , img
             [ class "large"
             , src (urlPrefix ++ "large/" ++ model.selectedUrl)
@@ -80,15 +98,45 @@ photoArray =
     Array.fromList initialModel.photos
 
 
+viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumb =
     img
         [ src (urlPrefix ++ thumb.url)
         , classList [ ( "selected", selectedUrl == thumb.url ) ]
-        , onClick { description = "ClickedPhoto", data = thumb.url }
+        , onClick (ClickedPhoto thumb.url)
         ]
         []
+
+
+viewSizeChooser : ThumbnailSize -> Html Msg
+viewSizeChooser size =
+    label []
+        [ input [ type_ "radio", name "size", onClick (ClickedSize size) ] []
+        , text (sizeToString size)
+        ]
+
+
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+    case size of
+        Small ->
+            "small"
+
+        Medium ->
+            "med"
+
+        Large ->
+            "large"
 
 
 urlPrefix : String
 urlPrefix =
     "http://elm-in-action.com/"
+
+getPhotoUrl: Int -> String
+getPhotoUrl index =
+    case Array.get index photoArray of
+        Just photo ->
+            photo.url
+        Nothing ->
+            ""
